f4bb22f205042df0d85450633742c8554df1a205
dmunter2 <devon.munter1@gmail.com>
2019-05-22 18:51:34 -0700

    object-final

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,34 +11,84 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+    "id": 0,
+    "name": "Example",
+    "email": "examples@you.edu",
+    "gender": "F"
 }
 
 // Write your intern objects here:
+const Mitzi = {
+    "id": '1',
+    "name": "Mitzi",
+    "email": 'mmelloy0@psu.edu',
+    'gender': 'F'
+}
+
+const Kennan = {
+    'id': '2',
+    'name': 'Kennan',
+    'email': 'kdiben1@tinypic.com',
+    'gender': 'M',
+    speak: function() {
+        console.log('Hi my name is Kennan!');
+    }
+
+}
+
+
+const Keven = {
+    'id': '3',
+    'name': 'Keven',
+    'email': 'kmummery2@wikimedia.org',
+    'gender': 'M'
+}
+
+const Gannie = {
+    'id': '4',
+    'name': 'Gannie',
+    'email': 'gmartinson3@illinois.edu',
+    'gender': 'm'
+}
+
+const Antonietta = {
+    'id': '5',
+    'name': 'Antonietta',
+    'email': 'adaine5@samsung.com',
+    'gender': 'F',
+    multiply: function(num1, num2) {
+        result1 = num1 * num2
+        return result1;
+    }
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(Mitzi.name);
 
 // Kennan's ID
+console.log(Kennan.id);
 
 // Keven's email
+console.log(Keven.email);
 
 // Gannie's name
+console.log(Gannie.name);
 
 // Antonietta's Gender
+console.log(Antonietta.gender);
+
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(Kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+console.log(Antonietta.multiply(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -49,7 +99,10 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+    name: 'susan',
+    age: 70,
+}
 
 // Log the parent object's name
 
@@ -61,4 +114,4 @@ const parent = {}
 
 // Have the child speak
 
-// Have the grandchild speak
+// Have the grandchild speakn

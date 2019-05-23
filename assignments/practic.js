8cb42eae8864d2cdf2a936f9c92baa729fe4bc88
dmunter2 <devon.munter1@gmail.com>
2019-05-22 21:53:19 -0700

    function-conversaion

---
+++ assignments/practic.js
@@ -0,0 +1,26 @@
+let myFunction = function() {
+    console.log("Function was invoked!");
+};
+
+myFunction();
+
+
+let anotherFunction = function(param) {
+    return param;
+};
+
+anotherFunction("Example");
+
+
+let add = function(param1, param2) {
+    return param1 + param2;
+};
+
+add(1, 2);
+
+
+
+let subtract = function(param1, param2) {
+    return param1 - param2;
+};
+subtract(1, 2);
